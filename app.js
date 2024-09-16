b7dd2242dad643875aa1356599b2171fcd46dc1b
ahmadrizalkurniawan <6182001023@student.unpar.ac.id>
2024-09-16 20:10:20 +0700

    Update: Implementasi API Google
    - Menambahkan library axios: meminta HTTP dari klien dan server untuk menerjemahkan teks menggunakan API Google.
    
    - Menambahkan Fungsi Button "Translate text" di Sisi Klien dan Server.

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
+const axios = require('axios');
 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('views', path.join(__dirname, 'views'));
@@ -12,6 +16,30 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
   res.render('translation_app');
 });
+
+app.post('/translate', async (req, res) => {
+  const { source_language, target_language, q } = req.body;
+
+  try {
+    const response = await axios.post(`https://translation.googleapis.com/language/translate/v2?key=${process.env.GOOGLE_TRANSLATE_API_KEY}`, {
+      q: q, // teks yang mau ditranslate
+      source: source_language,
+      target: target_language,
+      format: 'text'
+    });
+
+    res.json({ translated_text: response.data.data.translations[0].translatedText });
+
+  } catch (error) {
+    console.error('Error translating text:', {
+      message: error.message,
+      response: error.response ? error.response.data : null,
+      config: error.config
+    });
+    res.status(500).send('Translation error');
+  }
+});
+
 
 app.listen(8080, () => {
   console.log('Server running on port 8080');
--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,20 @@
 const fromText = document.querySelector(".from-text"),
-toText = document.querySelector(".to-text"),
-exchageIcon = document.querySelector(".exchange"),
-selectTag = document.querySelectorAll("select");
+    toText = document.querySelector(".to-text"),
+    exchageIcon = document.querySelector(".exchange"),
+    selectTag = document.querySelectorAll("select");
 translateBtn = document.querySelector("button"),
 
-selectTag.forEach((tag, id) => {
-    for (let country_code in countries) {
-        let selected = id == 0 ? country_code == "id-ID" ? "selected" : "" : country_code == "en-GB" ? "selected" : "";
-        let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
-        tag.insertAdjacentHTML("beforeend", option);
-    }
-});
+    selectTag.forEach((tag, id) => {
+        for (let country_code in countries) {
+            let selected = id == 0 ? country_code == "id-ID" ? "selected" : "" : country_code == "en-GB" ? "selected" : "";
+            let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
+            tag.insertAdjacentHTML("beforeend", option);
+        }
+    });
 
 exchageIcon.addEventListener("click", () => {
     let tempText = fromText.value,
-    tempLang = selectTag[0].value;
+        tempLang = selectTag[0].value;
     fromText.value = toText.value;
     toText.value = tempText;
     selectTag[0].value = selectTag[1].value;
@@ -22,11 +22,32 @@ exchageIcon.addEventListener("click", () => {
 });
 
 fromText.addEventListener("keyup", () => {
-    if(!fromText.value) {
-        toText.value = "";
+    if (!fromText.value) {
+        toText.value = "nothing";
     }
 });
 
-translateBtn.addEventListener("click", () => {
 
+
+translateBtn.addEventListener("click", async () => {
+    const fromLang = selectTag[0].value;
+    const toLang = selectTag[1].value;
+    const textToTranslate = fromText.value;
+
+    if (textToTranslate) {
+        try {
+            const response = await axios.post('/translate', {
+                q: textToTranslate,
+                source_language: fromLang,
+                target_language: toLang,
+                format: textToTranslate
+            });
+
+            const translatedText = response.data.translated_text;
+            toText.value = translatedText;
+        } catch (error) {
+            console.error('Error translating text:', error.response ? error.response.data : error.message);
+        }
+    }
 });
+
